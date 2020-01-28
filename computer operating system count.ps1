fa602245dc4c642ac472748ec3adac58a5a00255
Witt Allen PVL <pvl_wallen@nsm-seating.com>
2020-01-28 11:52:22 -0600

    2 scripts

---
+++ computer operating system count.ps1
@@ -0,0 +1,2 @@
+﻿$win8 = Get-ADComputer -Filter {(OperatingSystem -like "*8*") -and (Enabled -eq "True")} -Properties OperatingSystem | Sort Name
+$win8 | group operatingsystem
---
+++ computer when created count.ps1
@@ -0,0 +1,16 @@
+﻿$enabledComputers = Get-ADComputer -Filter {(Enabled -eq "True")} -Properties WhenCreated,operatingsystem | Sort Name
+
+$olderThan2016 = @()
+$olderThan2015 = @()
+
+foreach ($computer in $enabledComputers) {
+    if ($computer.WhenCreated -lt (Get-Date "1/1/2016 00:00:01")) {
+        $olderThan2016 += $computer        
+    }
+    if ($computer.WhenCreated -lt (Get-Date "1/1/2015 00:00:01")) {
+        $olderThan2015 += $computer        
+    }
+}
+
+$olderThan2016.Count
+$olderThan2015.Count
