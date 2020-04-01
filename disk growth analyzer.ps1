cb3b529db59186bf436dcc49fdda5252ef5335c2
Witt Allen PVL <pvl_wallen@nsm-seating.com>
2020-04-01 16:57:46 -0500

    Changed $ServerFQDN to param so it can be ran from the shell

--- disk growth analyzer.ps1
+++ disk growth analyzer.ps1
@@ -1,4 +1,4 @@
-$server = "server01.fqdn.local"
+param($ServerFQDN)
 $IntervalInMinutes = 5
 <#
 C drive is 23.0 GB free
@@ -6,6 +6,6 @@ D drive 51.0 GB free
 E drive 284 GB free
 #>
 for ($i=0; $i -lt 16; $i++) {
-    Invoke-Command -ComputerName $server {Get-PSDrive C} | Select-Object PSComputerName,Used,Free
+    Invoke-Command -ComputerName $ServerFQDN {Get-PSDrive C} | Select-Object PSComputerName,Used,Free
     Start-Sleep -s ($IntervalInMinutes*60) # Every x minutes
 }
