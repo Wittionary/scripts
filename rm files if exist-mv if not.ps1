2ad9f23f0ba8b90332fd1742d018697b31364760
Witt <Wittionary@users.noreply.github.com>
2020-05-20 09:33:49 -0500

    Added way to track which files were moved and deleted

--- rm files if exist-mv if not.ps1
+++ rm files if exist-mv if not.ps1
@@ -1,18 +1,24 @@
 # Use for incomplete mass-move or mass-copy file operations
+param (
+    $DirectoryFilesExist = "D:\Documents\TpyoedFolderName\",
+    $DirectoryFilesShouldExist = "D:\Documents\FolderName\"
+)
 
-$DirectoryFilesExist = "D:\Documents\TpyoedFolderName\"
-$DirectoryFilesShouldExist = "D:\Documents\FolderName\"
 $FilesRemoved = 0
 $FilesMoved = 0
 $Files = Get-ChildItem $DirectoryFilesExist*
 
+$FilesRemovedList = @()
+$FilesMovedList = @()
 
 foreach ($File in $Files) {
     if (test-path "$DirectoryFilesShouldExist$($File.name)") {
-        Remove-Item $file
+        Remove-Item $File
+        $FilesRemovedList += $File
         $FilesRemoved++
     } else {
-        Move-Item $file -Destination $DirectoryFilesShouldExist
+        Move-Item $File -Destination $DirectoryFilesShouldExist
+        $FilesMovedList += $File
         $FilesMoved++
     }
 }
