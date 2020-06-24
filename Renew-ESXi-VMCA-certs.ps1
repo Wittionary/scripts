e27771902705610a62098d8c95c459b964a869db
Witt <Wittionary@users.noreply.github.com>
2020-06-24 16:05:03 -0500

    Rewrote for public consumption

---
+++ Renew-ESXi-VMCA-certs.ps1
@@ -0,0 +1,36 @@
+param($vSphereServer,$vSphereAdmin,$CredentialPath)
+Install-Module vmware.powercli
+Import-Module vmware.powercli
+Set-PowerCLIConfiguration -scope User -participateinCEIP $false -InvalidCertificateAction Ignore -Confirm:$false
+
+function Renew-VMHostVMCACertificate {
+    [CmdletBinding()]
+    Param(
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()]
+        [Array]$ESXiHosts = (Get-VMHost | Where-Object {($_.ConnectionState -eq "Connected") -and ($_.PowerState -eq "PoweredOn")} | Select-Object -Property Name)
+    )
+
+    $ESXiHost = Get-Random $ESXiHosts
+
+    $hostid = Get-VMHost $ESXiHost.Name | Get-View
+    $hostParam = New-Object VMware.Vim.ManagedObjectReference[] (1)
+    $hostParam[0] = New-Object VMware.Vim.ManagedObjectReference
+    $hostParam[0].value = $hostid.moref.value
+    $hostParam[0].type = 'HostSystem'
+    $_this = Get-View -Id 'CertificateManager-certificateManager'
+    $_this.CertMgrRefreshCertificates_Task($hostParam)
+}
+
+if ($vSphereAdmin -match "wnsm.local") {
+    $vSphereUsername = $vSphereAdmin
+} else {
+    $vSphereUsername = "$($vSphereAdmin)@wnsm.local"
+}
+
+#Get-Content "\\tpn-702-adm01.wnsm.local\e$\creds\$($vSphereAdmin).txt"
+$vSpherePassword = Get-Content $CredentialPath | ConvertTo-SecureString
+$vSphereCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $vSphereUsername, $vSpherePassword
+
+Connect-VIServer -Server $vSphereServer -Credential $vSphereCreds
+Renew-VMHostVMCACertificate
