84d37e23e65e1cf43dcccfe128fe12d99c7a98e9
Witt <Wittionary@users.noreply.github.com>
2020-06-30 16:43:22 -0500

    Search AzureAD apps for references to onprem ADFS server

---
+++ Search AzureAD apps.ps1
@@ -0,0 +1,20 @@
+Param (
+    $SearchString = "adfs01.domain.tld"
+)
+
+Connect-AzureAd
+$Apps = Get-AzureADApplication -all $true
+
+
+$AppsReferencingOnpremADFS = @()
+foreach ($App in $Apps) {
+
+    if (($App.ReplyUrls -match $SearchString) -or ($App.LogoutUrl -match $SearchString) -or
+        ($App.IdentifierUris -match $SearchString) -or ($App.Homepage -match $SearchString) -or
+        ($App.ErrorUrl -match $SearchString)) {
+
+        $AppsReferencingOnpremADFS += $App
+    }
+}
+
+Write-Host $AppsReferencingOnpremADFS
