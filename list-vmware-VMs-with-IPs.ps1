426314ee7589db9318d45eae81775825fbbc1411
Witt <Wittionary@users.noreply.github.com>
2021-02-04 11:40:10 -0600

    List VMware VMs with their associated IPs

---
+++ list-vmware-VMs-with-IPs.ps1
@@ -0,0 +1,7 @@
+Connect-VIServer
+
+foreach ($VM in $VMs) {
+    $IPaddress = get-vmguest -VM $VM.Name | Select-Object IPaddress
+    $IPaddress = $IPaddress.IPAddress
+    Write-host "$($VM.Name) - $($IPaddress)"
+}
