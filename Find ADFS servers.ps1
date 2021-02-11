eda930e2442af5f4e4eeca97b16c5b06303012c0
Witt <Wittionary@users.noreply.github.com>
2021-02-11 14:29:37 -0600

    Commit All @ 02-11-2021 14:29:37

--- Find ADFS servers.ps1
+++ Find ADFS servers.ps1
@@ -1,7 +1,7 @@
 # https://docs.microsoft.com/en-us/powershell/module/activedirectory/get-addomaincontroller?view=winserver2012-ps
 # Currently only checks DCs for the ADFS role
 # TODO: search all servers in AD for ADFS role
-$allDCs = (Get-ADForest).Domains | %{ Get-ADDomainController -Filter * -Server $_ }
+$allDCs = (Get-ADForest).Domains | ForEach-Object { Get-ADDomainController -Filter * -Server $_ }
 
 foreach ($DC in $allDCs) {
     Get-WindowsFeature adfs-federation -ComputerName $DC.HostName
