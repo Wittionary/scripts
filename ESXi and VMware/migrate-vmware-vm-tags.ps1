05e31b1df6b96809b2ef3097dc19b0234575fa42
Witt <Wittionary@users.noreply.github.com>
2021-05-18 14:15:47 -0500

    Last change to fully migrate to prod tags

--- ESXi and VMware/migrate-vmware-vm-tags.ps1
+++ ESXi and VMware/migrate-vmware-vm-tags.ps1
@@ -1,10 +1,10 @@
 Connect-VIServer
-$ToBeRetagged = Get-TagAssignment | Where-Object {$_.Tag -match "Veeam Backups/veeam-creds:veeam_svc\+not-app-aware"}
+$ToBeRetagged = Get-TagAssignment | Where-Object {$_.Tag -match "Veeam Backups/LEGACY-veeam-creds:veeam_svc\+app-aware"}
 
 # Remove old tag first
 Remove-TagAssignment -TagAssignment $ToBeRetagged
 
 foreach ($Retag in $ToBeRetagged) {
     # Formerly did $(Get-VM ...) for Entity param
-    New-TagAssignment -Tag "LEGACY-veeam-creds:veeam_svc+not-app-aware" -Entity $Retag.Entity.Name
+    New-TagAssignment -Tag "veeam-creds:veeam_svc+app-aware" -Entity $Retag.Entity.Name
 }
