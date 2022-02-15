bc6d39ac36463e777a028daa621338da4d0ed65c
Witt Allen <Wittionary@users.noreply.github.com>
2022-02-15 13:20:52 -0600

    Added creating a new stack if existing stack not found

--- update-cloudformation-stack.ps1
+++ update-cloudformation-stack.ps1
@@ -1,6 +1,6 @@
 param (
     $BucketName = "witt-test-cf-templates",
-    $StackName = "witt-test-carsaver-prod-vpc",
+    $StackName = "witt-test-02-15-1311",
     $S3Object = "prod-vpc.yml",
     $DefaultRegion = "us-west-1"
 )
@@ -14,5 +14,12 @@ Set-DefaultAWSRegion -Region $DefaultRegion
 # Upload to S3
 Write-S3Object -BucketName $BucketName -File "$Env:git\pg\pts-witt-scratchpad\carsaver\NT220207a - CarSaver AWS CloudFormation\$S3Object"
 
-# Update CloudFormation stack that has already successfully deployed once
-Update-CFNStack -StackName "witt-test-carsaver-prod-vpc" -TemplateURL $S3ObjectURL -Region $DefaultRegion
+# Update (or create) CloudFormation stack
+$CurrentStack = Get-CFNStack -StackName $StackName -Region $DefaultRegion -ea SilentlyContinue
+if ($null -eq $CurrentStack) {
+    # Stack does not yet exist
+    # Create it with template
+    New-CFNStack -StackName $StackName -TemplateURL $S3ObjectURL -Region $DefaultRegion
+} else {
+    Update-CFNStack -StackName $StackName -TemplateURL $S3ObjectURL -Region $DefaultRegion
+}
