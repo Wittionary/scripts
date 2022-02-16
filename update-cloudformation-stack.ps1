6e6068404725676e736e479603e905a1664dfb15
Witt Allen <Wittionary@users.noreply.github.com>
2022-02-16 15:54:54 -0600

    No mo' needless errors!

--- update-cloudformation-stack.ps1
+++ update-cloudformation-stack.ps1
@@ -15,11 +15,15 @@ Set-DefaultAWSRegion -Region $DefaultRegion
 Write-S3Object -BucketName $BucketName -File "$Env:git\pg\pts-witt-scratchpad\carsaver\NT220207a - CarSaver AWS CloudFormation\$S3Object"
 
 # Update (or create) CloudFormation stack
-$CurrentStack = Get-CFNStack -StackName $StackName -Region $DefaultRegion -ErrorAction SilentlyContinue
+try {
+    $CurrentStack = Get-CFNStack -StackName $StackName -Region $DefaultRegion
+} catch { # ErrorAction isn't supported by AWS pwsh so try-catch is needed
+    $CurrentStack = $null
+}
 if ($null -eq $CurrentStack) {
     # Stack does not yet exist
     # Create it with template
-    New-CFNStack -StackName $StackName -TemplateURL $S3ObjectURL -Region $DefaultRegion
+    New-CFNStack -StackName $StackName -TemplateURL $S3ObjectURL -Region $DefaultRegion -Capability "CAPABILITY_NAMED_IAM"
 } else {
-    Update-CFNStack -StackName $StackName -TemplateURL $S3ObjectURL -Region $DefaultRegion
+    Update-CFNStack -StackName $StackName -TemplateURL $S3ObjectURL -Region $DefaultRegion -Capability "CAPABILITY_NAMED_IAM"
 }
