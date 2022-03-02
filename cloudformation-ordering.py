400f262b50e393900a90c9851f2a3172375f3570
Witt Allen <Wittionary@users.noreply.github.com>
2022-03-01 23:34:19 -0600

    Switched from pwsh to python

--- cloudformation-ordering.py
+++ cloudformation-ordering.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python
 # Intake a series of CloudFormation templates and spit out a MermaidJS diagram on the order they should be deployed
-[CmdletBinding()]
-param (
-    [Parameter(Mandatory=$true)]
-    [string]
-    $Path # Path to folder where templates are
-)
+import json
+import sys
+import os
+
+#directory = sys.argv[1]
+directory = "C:\Users\WittAllen\git\pg\pts-witt-scratchpad\carsaver\NT220207a - CarSaver AWS CloudFormation"
+print(f"Template directory: {directory}")
+
+# Get YAML files in directory
 
 # Read the Outputs of each template
 # Read the !ImportValues of each template
