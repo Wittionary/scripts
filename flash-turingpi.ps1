038d48d4f8ebbcc80c68947892422a0c339db788
Witt Allen <wittionary@users.noreply.github.com>
2022-04-30 16:58:34 -0500

    Working, fully automated script - assuming the filepaths are correct. Could download and install the pre-reqs if desired.

--- flash-turingpi.ps1
+++ flash-turingpi.ps1
@@ -11,11 +11,17 @@ param (
 
     $Timezone = "America/Chicago",
 
-    $UserdataFilepath = "E:\user-data"
+    $UserdataFilepath = "E:\user-data",
+    $RPiImagerInstallPath = "C:\Program Files (x86)\Raspberry Pi Imager",
+    $ImagePath = "D:\Witt\Downloads\hypriotos-rpi-v1.12.3.img\hypriotos-rpi-v1.12.3.img",
+    $DestinationDrive = "\\.\PhysicalDrive3"
 )
+$WorkingDir = (Get-Location).Path
 
+function Setup-Userdata {
+  $TempFilename = "$WorkingDir\temp-user-data"
 
-$Userdata = "#cloud-config
+  $Userdata = "#cloud-config
 # vim: syntax=yaml
 #
 
@@ -83,4 +89,102 @@ runcmd:
 #  # Activate WiFi interface
 #  - 'ifup wlan0'"
 
-Write-Host $Userdata | Out-File $UserdataFilepath -Force
+  Set-Clipboard $Userdata
+  Write-Host "Clipboard has been set to userdata." -ForegroundColor Blue
+  $Userdata | Out-File $TempFilename -Force
+  #$Userdata | Out-File $UserdataFilepath -Force
+  Move-Item $TempFilename $UserdataFilepath -Force
+
+  # Test if data was transferred properly
+  $Success = $false
+  $Success = Get-ChildItem $UserdataFilepath | Select-String $Hostname
+  if ($Success) {
+    Write-Host "Userdata written successfully!" -ForegroundColor Green
+  } else {
+    Write-Host "Hostname `"$Hostname`" not found in userdata.`nManually verify the user-data file is valid before proceeding." -ForegroundColor Red
+    Start-Sleep -Seconds 2
+    Write-Host "Hit " -NoNewline
+    Write-Host "[Enter] " -ForegroundColor Blue -NoNewline
+    $null = Read-Host "when the file is in place"
+    $null = Read-Host "Are you sure?"
+  }
+}
+
+function Prepare-Device {
+  # You should get a notification that a USB device is being setup. It should start with "BCM"
+  Write-Host "Waiting until device is ready..."
+  $Device = $null
+  while ($null -eq $Device) {
+    $Device = Get-PnpDevice -FriendlyName "BCM*" -PresentOnly -ErrorAction SilentlyContinue
+    if ($Device) {
+      Write-Host "Device found!" -ForegroundColor Green
+    }
+    else {
+      Start-Sleep -Seconds 5
+      Write-Host "Waiting until device is ready..." -ForegroundColor Blue
+    }
+  }
+  # Once that's done setting up, run rpiboot from elevated prompt
+  Write-Host "Waiting " -ForegroundColor Yellow -NoNewline
+  Write-Host "for rpiboot process to complete..."
+  & "D:\Program Files (x86)\Raspberry Pi\rpiboot.exe"
+  # $rpibootProcess = Get-Process -Name "rpiboot" -ErrorAction SilentlyContinue
+  # Wait-Process -Id $rpibootProcess.Id -ErrorAction SilentlyContinue
+}
+
+function ManualSteps-Preamble {
+  [console]::beep(640, 200)
+  [console]::beep(840, 200)
+  [console]::beep(940, 200)
+
+  Write-Host "Seat " -ForegroundColor Yellow -NoNewline
+  Write-Host "compute module into TPi node # 1"
+  Start-Sleep -Seconds 2
+  Write-Host "Plug " -ForegroundColor Yellow -NoNewline
+  Write-Host "power into TPi"
+  Start-Sleep -Seconds 2
+}
+
+function ManualSteps-Postamble {
+  Write-Host "Remove " -ForegroundColor Yellow -NoNewline
+  Write-Host "TPi power cable"
+  Start-Sleep -Seconds 2
+  Write-Host "Eject " -ForegroundColor Yellow -NoNewline
+  Write-Host "the compute module manually"
+  Start-Sleep -Seconds 2
+  Write-Host "Re-run script if you have more modules to flash ⚡"
+
+  [console]::beep(940, 200)
+  [console]::beep(840, 200)
+  [console]::beep(640, 200)
+}
+
+function Image-Device {
+  Write-Host "Writing " -ForegroundColor Yellow -NoNewline
+  Write-Host "image to USB drive..."
+  
+  Set-Location $RPiImagerInstallPath
+  & ".\rpi-imager-cli.cmd" --cli $ImagePath $DestinationDrive
+  Set-Location $WorkingDir
+}
+
+function Eject-UsbDrive {
+  # Eject USB drive
+  $DriveEject = New-Object -comObject Shell.Application
+  $HypriotDrive = Get-CimInstance win32_volume | Where-Object { $_.Label -eq "HypriotOS" }
+  $DriveEject.Namespace(17).ParseName($HypriotDrive.Name.Substring(0, 2)).InvokeVerb("Eject")
+  Write-Host "Drive ejected" -ForegroundColor Blue
+}
+
+# ------------- Where it runs
+# Download required programs
+
+ManualSteps-Preamble
+Prepare-Device
+Image-Device
+
+# After write is complete, open file explorer to the USB device that's mounted named HypriotOS and customize user-data file
+Setup-Userdata
+
+Eject-UsbDrive
+ManualSteps-Postamble
