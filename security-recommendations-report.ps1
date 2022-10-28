0996137feb5c31479c7897deadc7aeec055a7189
Witt Allen <Wittionary@users.noreply.github.com>
2022-10-28 08:18:10 -0500

    Finished. In a working state.

--- security-recommendations-report.ps1
+++ security-recommendations-report.ps1
@@ -1,8 +1,9 @@
 param (
-    #[Parameter(Mandatory=$true)]
-    $CsvFile = "C:\Users\WittAllen\Downloads\DecodeHealth-AzureSecurityCenterRecommendations_2022-10-27T16_56_18Z.csv",
+    [Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true)]
+    $CsvFile,
 
-    $ExportedFile = "C:\Users\WittAllen\Downloads\DecodeHealth-script-export-10-27-22.csv"
+	[Parameter(Position=1)]
+    $ExportedFile = "C:\Users\WittAllen\Downloads\DecodeHealth-script-export-$(Get-Date -Format yyyy-MM-dd).csv"
 )
 
 $Csv = Import-Csv -Path $CsvFile
@@ -18,14 +19,22 @@ foreach ($Record in $FilteredResults) {
 	$Record | Add-Member -Name "Resource Type" -Value $Record.resourceType -MemberType NoteProperty
 	$Record | Add-Member -Name "Benefits" -Value $Record.controls -MemberType NoteProperty
 	$Record | Add-Member -Name "Healthy" -Value $Record.state -MemberType NoteProperty
-	$Record = $Record | Select-Object "Business Impact","Recommendation","Resource Group","Resource Name","Resource Type","Benefits","Healthy"
-    Write-Output "$Record"
-
+    
 	if ($Record."Healthy" -eq "Healthy") {
 		$Record."Healthy" = "X"
 	} else {
 		$Record."Healthy" = " "
 	}
 }
+$FilteredResults = $FilteredResults | Select-Object "Business Impact","Recommendation","Resource Group","Resource Name","Resource Type","Benefits","Healthy"
 
-$FilteredResults | Export-Csv -Path $ExportedFile -Force
+Write-Host "$($PSStyle.Foreground.Yellow)First 3 records of report:$($PSStyle.Reset)"
+$FilteredResults | Select-Object -First 3
+
+$UserResponse = Read-Host -Prompt "Continue with export? Y/n"
+if (($UserResponse -eq "") -or ($UserResponse -match "y")) {
+	$FilteredResults | Export-Csv -Path $ExportedFile -Force
+	Write-Host "Report exported to:`n`t$ExportedFile"
+} else {
+	Write-Host "Report not exported."
+}
